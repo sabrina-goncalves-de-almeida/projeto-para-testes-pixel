bb5d026c1d33a709bd869272637e30170502a2c9
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-04-14 16:40:30 -0300

    feat(#1): criando botão simples

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Button from "./components/Button/index";
+
 function App() {
   return (
-    <h1>Hello World!</h1> 
+    <>
+      <h1>Hello World!</h1>
+      <Button>Olá Mundo!</Button>
+    </>
+     
   );
 }
 
---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css";
+
+function Button(){
+    return (
+        <button className={styles.choiceButton}>Olá Mundo!</button>
+    );
+}
+
+export default Button;
