c5f247902e5dc28b3dd7212aec6b756e667f1321
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-04-25 22:52:05 -0300

    feat(#2): acrescentando page 3

--- src/pages/phases/phase1/Phase1Page2/index.js
+++ src/pages/phases/phase1/Phase1Page2/index.js
@@ -9,7 +9,7 @@ function Phase1Page2() {
                 <h1 className={styles.titlePhase}>FASE 1</h1>
                 <PhaseScreen 
                     props={{phaseImage : phase1Image2, textOfQuest : "As telas dos computadores são divididas em uma grade de pequenos quadrados, podendo ser cada um de uma cor específica. Chamamos esses quadrados de elementos  de imagem ou pixels.", textAlt : "Imagem de um computador com representação de pixels em sua tela"}} 
-                    routesProperties={{buttonLeft : "/phase1screen1", buttonRight:""}}/>
+                    routesProperties={{buttonLeft : "/phase1screen1", buttonRight:"/phase1screen3"}}/>
             </body>
         </>
     );
